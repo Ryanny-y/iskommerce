07df74d4beeec304a645bbfbed6347aed39c9f56
ryanny <ryannymromero@gmail.com>
2026-03-19 01:15:09 +0800

    fix: Remove cors options

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -6,7 +6,6 @@ import { errorHandler } from "./utils/Errors";
 
 const app = express();
 
-app.options("*", corsMiddleWare);
 app.use(corsMiddleWare);
 app.use(cookieParser());
 app.use(express.json());
